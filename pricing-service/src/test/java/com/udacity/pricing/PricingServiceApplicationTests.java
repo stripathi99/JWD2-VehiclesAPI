77e354d9079a2e4c43efccfe83e7c35b3d946660
Shubham Tripathi <shubham.tripathi4@outlook.com>
2023-01-30 22:50:22 +0530

    Added unit test-case for PricingController.

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,32 @@
 package com.udacity.pricing;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.udacity.pricing.api.PricingController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(PricingController.class)
 public class PricingServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  private MockMvc mockMvc;
 
+  @Test
+  public void get_all_19_prices() throws Exception {
+    for (int vehicleId = 1; vehicleId < 20; vehicleId++) {
+      mockMvc.perform(get("/services/price?vehicleId=" + vehicleId))
+          .andExpect(status().isOk())
+          .andExpect(content().json("{}"))
+          .andExpect(jsonPath("$.vehicleId").value(vehicleId));
+    }
+  }
 }
