97a1e0a431c558048beed6dd67f9392420421879
Shubham Tripathi <shubham.tripathi4@outlook.com>
2023-01-31 02:06:59 +0530

    Updated Eureka discovery settings.

--- vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
@@ -1,36 +1,43 @@
 package com.udacity.vehicles;
 
-import com.udacity.vehicles.domain.manufacturer.Manufacturer;
-import com.udacity.vehicles.domain.manufacturer.ManufacturerRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.reactive.function.client.WebClient;
 
 /**
- * Launches a Spring Boot application for the Vehicles API,
- * initializes the car manufacturers in the database,
- * and launches web clients to communicate with maps and pricing.
+ * Launches a Spring Boot application for the Vehicles API, initializes the car manufacturers in the
+ * database, and launches web clients to communicate with maps and pricing.
  */
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableDiscoveryClient
 public class VehiclesApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(VehiclesApiApplication.class, args);
-    }
+  private LoadBalancerExchangeFilterFunction loadBalancerExchangeFilterFunction;
 
-    /**
-     * Initializes the car manufacturers available to the Vehicle API.
-     * @param repository where the manufacturer information persists.
-     * @return the car manufacturers to add to the related repository
-     */
+  public static void main(String[] args) {
+    SpringApplication.run(VehiclesApiApplication.class, args);
+  }
+
+  @Autowired
+  public void setLoadBalancerExchangeFilterFunction(
+      LoadBalancerExchangeFilterFunction loadBalancerExchangeFilterFunction) {
+    this.loadBalancerExchangeFilterFunction = loadBalancerExchangeFilterFunction;
+  }
+
+  /**
+   * Initializes the car manufacturers available to the Vehicle API.
+   *
+   * @param repository where the manufacturer information persists.
+   * @return the car manufacturers to add to the related repository
+   */
 //    @Bean
 //    CommandLineRunner initDatabase(ManufacturerRepository repository) {
 //        return args -> {
@@ -41,30 +48,31 @@ public class VehiclesApiApplication {
 //            repository.save(new Manufacturer(104, "Dodge"));
 //        };
 //    }
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+  /**
+   * Web Client for the maps (location) API
+   *
+   * @param endpoint where to communicate for the maps API
+   * @return created maps endpoint
+   */
+  @Bean(name = "maps")
+  public WebClient webClientMaps(@Value("${maps.endpoint}") String endpoint) {
+    return WebClient.create(endpoint);
+  }
 
-    /**
-     * Web Client for the maps (location) API
-     * @param endpoint where to communicate for the maps API
-     * @return created maps endpoint
-     */
-    @Bean(name="maps")
-    public WebClient webClientMaps(@Value("${maps.endpoint}") String endpoint) {
-        return WebClient.create(endpoint);
-    }
-
-    /**
-     * Web Client for the pricing API
-     * @param endpoint where to communicate for the pricing API
-     * @return created pricing endpoint
-     */
-    @Bean(name="pricing")
-    public WebClient webClientPricing(@Value("${pricing.endpoint}") String endpoint) {
-        return WebClient.create(endpoint);
-    }
-
+  /**
+   * Web Client for the pricing API
+   *
+   * @param endpoint where to communicate for the pricing API
+   * @return created pricing endpoint
+   */
+  @Bean(name = "pricing")
+  public WebClient webClientPricing(@Value("${pricing.endpoint}") String endpoint) {
+    // Implementation as per -> http://www.programmersought.com/article/5385639834
+    return WebClient.builder().filter(loadBalancerExchangeFilterFunction).baseUrl(endpoint).build();
+  }
 }
