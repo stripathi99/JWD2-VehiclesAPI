58b33248f1ba18292fbf29da0ba4f7810f582ed0
Shubham Tripathi <shubham.tripathi4@outlook.com>
2023-01-30 17:17:46 +0530

    Added delete car functionality.

--- vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -8,81 +8,75 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 /**
- * Implements the car service create, read, update or delete
- * information about vehicles, as well as gather related
- * location and price data when desired.
+ * Implements the car service create, read, update or delete information about vehicles, as well as
+ * gather related location and price data when desired.
  */
 @Service
 public class CarService {
 
-    private final CarRepository repository;
+  private final CarRepository repository;
 
-    private final MapsClient mapsClient;
-    private final PriceClient priceClient;
+  private final MapsClient mapsClient;
+  private final PriceClient priceClient;
 
-    public CarService(CarRepository repository, MapsClient mapsClient, PriceClient priceClient) {
-        /**
-         * TODO: Add the Maps and Pricing Web Clients you create
-         *   in `VehiclesApiApplication` as arguments and set them here.
-         */
-        this.repository = repository;
-        this.mapsClient = mapsClient;
-        this.priceClient = priceClient;
-    }
-
+  public CarService(CarRepository repository, MapsClient mapsClient, PriceClient priceClient) {
     /**
-     * Gathers a list of all vehicles
-     * @return a list of all vehicles in the CarRepository
+     * TODO: Add the Maps and Pricing Web Clients you create
+     *   in `VehiclesApiApplication` as arguments and set them here.
      */
-    public List<Car> list() {
-        return repository.findAll();
+    this.repository = repository;
+    this.mapsClient = mapsClient;
+    this.priceClient = priceClient;
+  }
+
+  /**
+   * Gathers a list of all vehicles
+   *
+   * @return a list of all vehicles in the CarRepository
+   */
+  public List<Car> list() {
+    return repository.findAll();
+  }
+
+  /**
+   * Gets car information by ID (or throws exception if non-existent)
+   *
+   * @param id the ID number of the car to gather information on
+   * @return the requested car's information, including location and price
+   */
+  public Car findById(Long id) {
+    Car car = repository.findById(id).orElseThrow(CarNotFoundException::new);
+    car.setPrice(priceClient.getPrice(id));
+    car.setLocation(mapsClient.getAddress(car.getLocation()));
+    return car;
+  }
+
+  /**
+   * Either creates or updates a vehicle, based on prior existence of car
+   *
+   * @param car A car object, which can be either new or existing
+   * @return the new/updated car is stored in the repository
+   */
+  public Car save(Car car) {
+    if (car.getId() != null) {
+      return repository.findById(car.getId())
+          .map(carToBeUpdated -> {
+            carToBeUpdated.setDetails(car.getDetails());
+            carToBeUpdated.setLocation(car.getLocation());
+            return repository.save(carToBeUpdated);
+          }).orElseThrow(CarNotFoundException::new);
     }
 
-    /**
-     * Gets car information by ID (or throws exception if non-existent)
-     * @param id the ID number of the car to gather information on
-     * @return the requested car's information, including location and price
-     */
-    public Car findById(Long id) {
-        Car car = repository.findById(id).orElseThrow(CarNotFoundException::new);
-        car.setPrice(priceClient.getPrice(id));
-        car.setLocation(mapsClient.getAddress(car.getLocation()));
-        return car;
-    }
+    return repository.save(car);
+  }
 
-    /**
-     * Either creates or updates a vehicle, based on prior existence of car
-     * @param car A car object, which can be either new or existing
-     * @return the new/updated car is stored in the repository
-     */
-    public Car save(Car car) {
-        if (car.getId() != null) {
-            return repository.findById(car.getId())
-                    .map(carToBeUpdated -> {
-                        carToBeUpdated.setDetails(car.getDetails());
-                        carToBeUpdated.setLocation(car.getLocation());
-                        return repository.save(carToBeUpdated);
-                    }).orElseThrow(CarNotFoundException::new);
-        }
-
-        return repository.save(car);
-    }
-
-    /**
-     * Deletes a given car by ID
-     * @param id the ID number of the car to delete
-     */
-    public void delete(Long id) {
-        /**
-         * TODO: Find the car by ID from the `repository` if it exists.
-         *   If it does not exist, throw a CarNotFoundException
-         */
-
-
-        /**
-         * TODO: Delete the car from the repository.
-         */
-
-
-    }
+  /**
+   * Deletes a given car by ID
+   *
+   * @param id the ID number of the car to delete
+   */
+  public void delete(Long id) {
+    Car car = repository.findById(id).orElseThrow(CarNotFoundException::new);
+    repository.delete(car);
+  }
 }
--- vehicles-api/src/main/resources/data.sql
+++ vehicles-api/src/main/resources/data.sql
@@ -1,29 +1,14 @@
-INSERT INTO
-        manufacturer
-    (name, code)
-        values
-    ('Audi', 100),
-    ('Chevrolet', 101),
-    ('Ford', 102),
-    ('BMW', 103),
-    ('Dodge', 104);
+INSERT INTO manufacturer (name, code)
+VALUES
+  ('Audi', 100),
+  ('Chevrolet', 101),
+  ('Ford', 102),
+  ('BMW', 103),
+  ('Dodge', 104);
 
-INSERT INTO car (ID,
-         CONDITION,
-         CREATED_AT,
-         BODY,
-         ENGINE,
-         EXTERNAL_COLOR,
-         FUEL_TYPE,
-         MILEAGE,
-         MODEL,
-         MODEL_YEAR,
-         NUMBER_OF_DOORS,
-         PRODUCTION_YEAR,
-         LAT,
-         LON,
-         MODIFIED_AT,
-         MANUFACTURER_CODE
-     )
-        VALUES
-   (11, 'USED', CURRENT_TIMESTAMP, 'sedan', '3.6L V6', 'white', 'Gasoline', '32280', 'Impala', 2018, 4, 2018, '40.73061', '-73.935242', CURRENT_TIMESTAMP, 101);
+INSERT INTO car (ID, CONDITION, CREATED_AT, BODY, ENGINE, EXTERNAL_COLOR, FUEL_TYPE, MILEAGE, MODEL, MODEL_YEAR, NUMBER_OF_DOORS, PRODUCTION_YEAR, LAT, LON, MODIFIED_AT, MANUFACTURER_CODE)
+VALUES
+  (1, 'NEW', CURRENT_TIMESTAMP, 'sedan', '3.6L V6', 'white', 'Gasoline', '32280', 'Impala', 2018, 4, 2018, '40.73061', '-73.935242', CURRENT_TIMESTAMP, 101),
+  (2, 'NEW', CURRENT_TIMESTAMP, 'suv', '3.6L V6', 'grey', 'Gasoline', '32280', 'Impala', 2018, 4, 2018, '40.73061', '-73.935242', CURRENT_TIMESTAMP, 102),
+  (3, 'USED', CURRENT_TIMESTAMP, 'hatch-back', '3.6L V6', 'white', 'Gasoline', '32280', 'Impala', 2018, 4, 2018, '40.73061', '-73.935242', CURRENT_TIMESTAMP, 103),
+  (4, 'USED', CURRENT_TIMESTAMP, 'cross-country', '3.6L V6', 'white', 'Gasoline', '32280', 'Impala', 2018, 4, 2018, '40.73061', '-73.935242', CURRENT_TIMESTAMP, 104);
